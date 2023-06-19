1588384b9f8ff1c154e067bdf2fc734d8ac07640
Corneliu <29034380+mishimomei@users.noreply.github.com>
2023-06-19 11:36:28 +0300

    removed the 5 rounds logic, added 3 buttons for each player selection, added eventListener for buttons, transfered game output from console.log to DOM, added lastgame result display and total game score with a limit of 5 points per player,

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
+const resultContainer = document.querySelector('.resultContainer');
+//const computerChoiceContainer = document.querySelector('.computerChoiceContainer');
+//const playerSelectionContainer = document.querySelector('.playerSelectionContainer');
+const resultRowOne = document.querySelector('#resultRowOne');
+//const resultRowTwo = document.querySelector('#resultRowTwo');
+const resultRowThree = document.querySelector('#resultRowThree');
+const resultRowFour = document.querySelector('#resultRowFour');
+const scoreContainer = document.querySelector('#scoreContainer');
+
+// declaring round counter
+let counter = 1;
+// declaring score counters
+let computerScore = 0;
+let playerScore = 0;
+let tieScore = 0;
+
 // the function for getting the Computer Choice
 function getComputerChoice() {
 
@@ -14,61 +30,49 @@ function getComputerChoice() {
     }
 }
 
-// the function for getting playerSelection and making the insertion case insensitive
-function getPlayerSelection() {
-    let playerSelection = prompt("Pick your fighter - rock, paper, scissors!", "");
-    return (playerSelection.toLowerCase());
-}
-
 // the function that plays 1 round of Rock-Paper-Scissors after taking 2 parameters, the playerSelection and computerSelection
 function playRound(playerSelection, computerSelection) {
 
     // announcing the fighters    
-    console.log("Computer Choice: " + computerSelection);
-    console.log("Player Selection: " + playerSelection);
+    //computerChoiceContainer.textContent = "Computer Choice: " + computerSelection;
+    //playerSelectionContainer.textContent = "Player Selection: " + playerSelection;
     // empty line for visibility
-    console.log("");
+    //console.log("");
 
     // declaring gameResult variable for Wins counter in game
     let gameResult;
 
     if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            console.log("Rock beats Scissors");
-            console.log("Player Won, Congratulations!");
+            resultRowOne.textContent = "Player WIN : Rock beats Scissors";
             gameResult = "playerWin";
         } else if (computerSelection === "paper") {
-            console.log("Player LOST, Paper beats Rock!");
+            resultRowOne.textContent = "Player LOSE : Paper beats Rock!";
             gameResult = "computerWin";
         } else {            // computerSelection === "rock"
-            console.log("Rock VS Rock");
-            console.log("It's a TIE!!!");
+            resultRowOne.textContent = "It's a TIE : Rock VS Rock";
             gameResult = "tie";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            console.log("Paper beats Rock");
-            console.log("Player Won, Congratulations!");
+            resultRowOne.textContent = "Player WIN : Paper beats Rock";
             gameResult = "playerWin";
         } else if (computerSelection === "scissors") {
-            console.log("Player LOST, Scissors beat Paper!");
+            resultRowOne.textContent = "Player LOSE : Scissors beat Paper!";
             gameResult = "computerWin";
         } else {            // computerSelection === "paper"
-            console.log("Paper VS Paper");
-            console.log("It's a TIE!!!");
+            resultRowOne.textContent = "It's a TIE : Paper VS Paper";
             gameResult = "tie";
         }
     } else {                // playerSelection === "scissors"
         if (computerSelection === "paper") {
-            console.log("Scissors beat Paper");
-            console.log("Player Won, Congratulations!");
+            resultRowOne.textContent = "Player WIN : Scissors beat Paper";
             gameResult = "playerWin";
         } else if (computerSelection === "rock") {
-            console.log("Player LOST, Rock beats Scissors!");
+            resultRowOne.textContent = "Player LOSE : Rock beats Scissors!";
             gameResult = "computerWin"
         } else {            // computerSelection === "paper"
-            console.log("Scissors VS Scissors");
-            console.log("It's a TIE!!!");
+            resultRowOne.textContent = "It's a TIE : Scissors VS Scissors";
             gameResult = "tie";
         }
     }
@@ -77,44 +81,43 @@ function playRound(playerSelection, computerSelection) {
     return (gameResult);
 }
 
-// the 5 rounds Game function
-function game() {
-    // declaring round counter
-    let counter = 1;
-    // declaring score counters
-    let computerScore = 0;
-    let playerScore = 0;
-    let tieScore = 0;
+// the Game function
+function game(playerSelection) {
 
-    // rounds loop
-    while (counter <= 5) {
-        console.log("Game Number: " + counter);
-        let roundResult = playRound(getPlayerSelection(), getComputerChoice());
-        if (roundResult === "playerWin") {
-            playerScore++;
-        } else if (roundResult === "computerWin") {
-            computerScore++;
-        } else {
-            tieScore++;
-        }
-        // space for adding visibility after each game round
-        console.log("");
-        counter++;
+    scoreContainer.textContent = counter;
+    let roundResult = playRound(playerSelection, getComputerChoice());
+    if (roundResult === "playerWin") {
+        playerScore++;
+    } else if (roundResult === "computerWin") {
+        computerScore++;
+    } else {
+        tieScore++;
     }
 
-    // space for announcing winner
-    console.log("");
+
     // conditional for winner announcement
     if (playerScore > computerScore) {
-        console.log("Player WON with a score of " + playerScore + " to " + computerScore);
-        console.log("Number of ties: " + tieScore);
+        resultRowThree.textContent = ("Player WINS with a score of " + playerScore + " to " + computerScore);
+        resultRowFour.textContent = ("Number of ties: " + tieScore);
     } else if (computerScore > playerScore) {
-        console.log("Player LOST with a score of " + playerScore + " to " + computerScore);
-        console.log("Number of ties: " + tieScore);
+        resultRowThree.textContent = ("Player LOSES with a score of " + playerScore + " to " + computerScore);
+        resultRowFour.textContent = ("Number of ties: " + tieScore);
     } else {
-        console.log("It's a TIE!!! score is " + playerScore + " to " + computerScore);
-        console.log("Number of ties: " + tieScore);
+        resultRowThree.textContent = ("It's a TIE!!! score is " + playerScore + " to " + computerScore);
+        resultRowFour.textContent = ("Number of ties: " + tieScore);
     }
 }
 
-game();
+//game();
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+        if (playerScore < 5 && computerScore < 5) {
+            game(button.id);
+            counter++;
+        }
+    });
+});
